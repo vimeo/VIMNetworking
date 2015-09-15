819f55e95d743b1841e4e4f9f55b5ac4e7128c07
Alfie Hanssen <alfiehanssen@gmail.com>
2015-09-15 14:55:04 -0400

    error reporting

--- VIMNetworking/Networking/ErrorReporting/NSError+VIMNetworking.m
+++ VIMNetworking/Networking/ErrorReporting/NSError+VIMNetworking.m
@@ -9,8 +9,22 @@
 #import "NSError+VIMNetworking.h"
 #import "AFURLResponseSerialization.h"
 
+// A numeric error code that provides direction as to why the error occurred,
+// We hope that in the future these can be sunset in favor of using only VimeoErrorCodeKey (see below)
 NSString * const VimeoErrorCodeHeaderKey = @"Vimeo-Error-Code";
-NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
+NSString * const VimeoErrorCodeKeyLegacy = @"VimeoErrorCode";
+
+// A numeric error code that provides direction as to why the error occurred
+NSString * const VimeoErrorCodeKey = @"error_code";
+
+// A list of numeric error codes that indicate which parameters are causing the request to fail
+NSString * const VimeoInvalidParametersKey = @"invalid_parameters";
+
+// A developer-facing message
+NSString * const VimeoDeveloperMessageKey = @"developer_message";
+
+// A user-facing message
+NSString * const VimeoUserMessageKey = @"error";
 
 @implementation NSError (VIMNetworking)
 
@@ -27,7 +41,7 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     if (urlResponse && [urlResponse statusCode] == HTTPErrorCodeUnauthorized)
     {
         NSString *header = urlResponse.allHeaderFields[@"WWW-Authenticate"];
-        if ([header isEqualToString:@"Bearer error=\"invalid_token\""])
+        if (header && [header isEqualToString:@"Bearer error=\"invalid_token\""])
         {
             return YES;
         }
@@ -48,12 +62,12 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
 
 - (BOOL)isUploadQuotaDailyExceededError
 {
-    return [self serverErrorCode] == VIMErrorCodeUploadDailyQuotaExceeded;
+    return [self vimeoErrorCode] == VIMErrorCodeUploadDailyQuotaExceeded;
 }
 
 - (BOOL)isUploadQuotaStorageExceededError
 {
-    return [self serverErrorCode] == VIMErrorCodeUploadStorageQuotaExceeded;
+    return [self vimeoErrorCode] == VIMErrorCodeUploadStorageQuotaExceeded;
 }
 
 #pragma mark - Utilities
@@ -69,9 +83,9 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     return NSNotFound;
 }
 
-- (NSInteger)serverErrorCode
+- (NSInteger)vimeoErrorCode
 {
-    NSNumber *errorCodeNumber = self.userInfo[VimeoErrorCodeKey];
+    NSNumber *errorCodeNumber = self.userInfo[VimeoErrorCodeKeyLegacy];
     if (errorCodeNumber)
     {
         NSInteger errorCode = [errorCodeNumber integerValue];
@@ -92,7 +106,7 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     }
     
     NSDictionary *json = [self errorResponseBodyJSON];
-    errorCodeNumber = [json objectForKey:@"error_code"];
+    errorCodeNumber = [json objectForKey:VimeoErrorCodeKey];
     if (errorCodeNumber)
     {
         return [errorCodeNumber integerValue];
@@ -101,9 +115,9 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     return NSNotFound;
 }
 
-- (NSInteger)serverInvalidParametersFirstErrorCode
+- (NSInteger)vimeoInvalidParametersFirstErrorCode
 {
-    NSArray *errorCodes = [self serverInvalidParametersErrorCodes];
+    NSArray *errorCodes = [self vimeoInvalidParametersErrorCodes];
     if (errorCodes && [errorCodes count])
     {
         NSString *firstCode = [errorCodes firstObject];
@@ -114,18 +128,18 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     return NSNotFound;
 }
 
-- (NSArray *)serverInvalidParametersErrorCodes
+- (NSArray *)vimeoInvalidParametersErrorCodes
 {
     NSMutableArray *errorCodes = [NSMutableArray new];
    
     NSDictionary *json = [self errorResponseBodyJSON];
     if (json)
     {
-        NSArray *invalidParameters = json[@"invalid_parameters"];
+        NSArray *invalidParameters = json[VimeoInvalidParametersKey];
         
         for (NSDictionary *errorJSON in invalidParameters)
         {
-            NSString *errorCode = errorJSON[@"error_code"];
+            NSString *errorCode = errorJSON[VimeoErrorCodeKey];
             if (errorCode)
             {
                 [errorCodes addObject:@([errorCode integerValue])];
@@ -136,11 +150,11 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     return errorCodes;
 }
 
-- (NSString *)serverInvalidParametersErrorCodesString
+- (NSString *)vimeoInvalidParametersErrorCodesString
 {
     NSString *result = nil;
     
-    NSArray *errorCodes = [self serverInvalidParametersErrorCodes];
+    NSArray *errorCodes = [self vimeoInvalidParametersErrorCodes];
     if (errorCodes && [errorCodes count])
     {
         result = @"";
@@ -161,12 +175,38 @@ NSString * const VimeoErrorCodeKey = @"VimeoErrorCode";
     return result;
 }
 
+- (NSString *)vimeoUserMessage
+{
+    NSString *message = nil;
+    
+    NSDictionary *json = [self errorResponseBodyJSON];
+    if (json)
+    {
+        message = json[VimeoUserMessageKey];
+    }
+    
+    return message;
+}
+
+- (NSString *)vimeoDeveloperMessage
+{
+    NSString *message = nil;
+    
+    NSDictionary *json = [self errorResponseBodyJSON];
+    if (json)
+    {
+        message = json[VimeoDeveloperMessageKey];
+    }
+    
+    return message;
+}
+
 - (NSDictionary *)errorResponseBodyJSON
 {
     NSData *data = self.userInfo[AFNetworkingOperationFailingURLResponseDataErrorKey];
     if (data)
     {
-        NSError *error;
+        NSError *error = nil;
         NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
         
         if (json && !error)
