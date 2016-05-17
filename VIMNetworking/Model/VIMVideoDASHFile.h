d8c8e0565e2a27bd93fa5b9dc6bf6b1d3b2405b2
Nicole Lehrer <nicole@vimeo.com>
2016-05-16 23:21:12 -0400

    added file for DASH file streaming

---
+++ VIMNetworking/Model/VIMVideoDASHFile.h
@@ -0,0 +1,15 @@
+//
+//  VIMVideoDASHFile.h
+//  Vimeo
+//
+//  Created by Lehrer, Nicole on 5/16/16.
+//  Copyright © 2016 Vimeo. All rights reserved.
+//
+
+@import Foundation;
+
+#import "VIMVideoPlayFile.h"
+
+@interface VIMVideoDASHFile : VIMVideoPlayFile
+
+@end
---
+++ VIMNetworking/Model/VIMVideoDASHFile.m
@@ -0,0 +1,13 @@
+//
+//  VIMVideoDASHFile.m
+//  Vimeo
+//
+//  Created by Lehrer, Nicole on 5/16/16.
+//  Copyright © 2016 Vimeo. All rights reserved.
+//
+
+#import "VIMVideoDASHFile.h"
+
+@implementation VIMVideoDASHFile
+
+@end
