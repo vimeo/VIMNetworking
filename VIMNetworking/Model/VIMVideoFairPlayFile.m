0b6a479ff1df936475c80a29f030376118db0232
Gavin King <gavin@vimeo.com>
2016-08-03 10:09:43 -0400

    more comments

--- VIMNetworking/Model/VIMVideoFairPlayFile.m
+++ VIMNetworking/Model/VIMVideoFairPlayFile.m
@@ -28,15 +28,4 @@
 
 @implementation VIMVideoFairPlayFile
 
-#pragma mark - VIMMappable
-
-- (NSDictionary *)getObjectMapping
-{
-    NSMutableDictionary *mapping = [[super getObjectMapping] mutableCopy];
-    
-    [mapping addEntriesFromDictionary:@{@"certificate_link": @"certificateLink", @"license_link": @"licenseLink"}];
-    
-    return mapping;
-}
-
 @end
